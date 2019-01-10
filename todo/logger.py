81a58975ff3c3b864fe70c4893617621f0e33811
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-01-10 16:21:55 +0400

    Create classes Business and Logger

--- todo/logger.py
+++ todo/logger.py
@@ -1,17 +1,24 @@
 from typing import List
 from attr import attrs, attrib
 
+
 @attrs(slots=True)
-class Bussines:
+class Business:
     date_begin: str = attrib(default='')
     time_begin: str = attrib(default='')
     date_end: str = attrib(default='')
     time_end: str = attrib(default='')
-
+    case: str = attrib(default='')
 
 
 class Logger:
     def __init__(self, file_name: str)-> None:
         self.file_name = file_name
 
-    def load_from_files(self)-> List:
+    @staticmethod
+    def parse_list(lines: List[str])-> List[Business]:
+        pass
+
+    def load_from_files(self)-> List[Business]:
+        lines = List[str]
+        return self.parse_list(lines)
