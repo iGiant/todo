2f2eedbc1a0e675bf262daef13a9ab626cdaceb4
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-01-15 14:02:45 +0400

    реформат write_to_file

--- todo/logger.py
+++ todo/logger.py
@@ -73,11 +73,9 @@ class Logger:
         :param lines: Список дел, которые необходимо сохранить
         :return: None
         """
+        sort_lines = sorted(lines, key=lambda case: f'{case.date_begin[YEAR]}{case.date_begin[MONTH]}'
+        f'{case.date_begin[DAY]}{case.time_begin[HOUR]}{case.time_begin[MINUTE]}')
         with open(self.file_name, 'w', encoding='utf8') as write_file:
-            sort_lines = sorted(lines,
-                                key=lambda case: f'{case.date_begin[YEAR]}'
-                                f'{case.date_begin[MONTH]}{case.date_begin[DAY]}'
-                                f'{case.time_begin[HOUR]}{case.time_begin[MINUTE]}')
             for line in sort_lines:
                 write_data = (f'{line.date_begin} {line.time_begin} {TIMES_SEPARATOR} '
                               f'{line.date_end} {line.time_end} {CASE_SEPARATOR} {line.case}'
