ce66ce18cca28d612febcf789a636eb6421fe956
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-01-18 10:39:15 +0400

    Реформат и аннотации

--- todo/threads.py
+++ todo/threads.py
@@ -5,41 +5,69 @@ from tkinter import Label
 from .settings import FONT, PAUSE
 
 
-def _get_fixit_length_text(label_ : Label, text_: str, width: int)-> str:
-    label_['text'] = text_
-    end = len(text_)
-    while label_.winfo_reqwidth() > width:
-        end -= 1
-        label_['text'] = text_[:end]
-    return text_[:end]
-
-
-def _scrool_label(label: Label, date: str, text: str, length: int)-> None:
-
-    def _show_text(text_, time):
-        label['text'] = text_
+def _scrool_label(label: Label, date: str, text: str, width_label: int)-> None:
+    """
+    выполняемая в отдельном потоке функция про прокрутки текста
+    :param label: виджет для вывода текста
+    :param date: часть строки для вывода
+    :param text: текст для вывода
+    :param width_label: ширина виджета Label в пикселях
+    :return:
+    """
+    def _show_text(show_text, time):
+        """
+        Помещает текст в виджет Label и делает паузу в time секунд
+        :param show_text: Выводимы текст
+        :param time:
+        :return:
+        """
+        label['text'] = show_text
         sleep(time)
 
+    def _get_fixit_length_text(test_label: Label, full_text: str, width: int)-> str:
+        """
+        Возвращает текст для формирование Label нужной длины
+        :param test_label: тестовый виджет Label для проверки
+        :param full_text: текст, часть которого необходимо вывести
+        :param width: Максимальная ширина виджета
+        :return: срез текста для вывода через виджет Label
+        """
+        test_label['text'] = full_text
+        end = len(full_text)
+        while test_label.winfo_reqwidth() > width:
+            end -= 1
+            test_label['text'] = full_text[:end]
+        return full_text[:end]
+
+    # _scrool_label()
     temp_label = Label(None, font=FONT)
 
-    if _get_fixit_length_text(temp_label, f'{date}: {text}', length) == f'{date}: {text}':
+    if _get_fixit_length_text(temp_label, f'{date}: {text}', width_label) == f'{date}: {text}':
         label['text'] = f'{date}: {text}'
         return None
 
     start = 0
 
-    _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', length), 5 * PAUSE)
+    _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', width_label), 5 * PAUSE)
     while True:
-        if _get_fixit_length_text(temp_label, f'{date}: {text[start:]}', length) == f'{date}: {text[start:]}':
+        if _get_fixit_length_text(temp_label, f'{date}: {text[start:]}', width_label) == f'{date}: {text[start:]}':
             _show_text(f'{date}: {text[start:]}', 7 * PAUSE)
             start = 0
-            _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', length), 5 * PAUSE)
+            _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', width_label), 5 * PAUSE)
         else:
-            _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', length), PAUSE)
+            _show_text(_get_fixit_length_text(temp_label, f'{date}: {text[start:]}', width_label), PAUSE)
         start += 1
 
 
 def start_scrool_label(label: Label, date: str, text: str, length: int)-> None:
+    """
+
+    :param label:
+    :param date:
+    :param text:
+    :param length:
+    :return:
+    """
     thread = Thread(target=_scrool_label, args=(label, date, text, length))
     thread.daemon = True
     thread.start()
